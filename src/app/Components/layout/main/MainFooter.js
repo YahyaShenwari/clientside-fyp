3d4811ff6f8429c189248a1c16af7a2139320fe5
YahyaShenwari <yahyshewnari6@gmail.com>
2020-08-11 19:47:14 +0500

    my very first commit

---
+++ src/app/Components/layout/main/MainFooter.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const MainFooter = () => {
+  return (
+    <div
+      className="kt-footer  kt-footer--extended  kt-grid__item"
+      id="kt_footer"
+      style={{ backgroundImage: "url(/media/bg/bg-2.jpg)" }}
+    >
+      <div className="kt-container">
+        <div className="kt-footer__bottom">
+          <div>
+            <div className="kt-footer__wrapper">
+              <div className="kt-footer__logo">
+                <div className="kt-footer__copyright">
+                  {new Date().getFullYear()}&nbsp;&copy;&nbsp;
+                  <Link to="/">
+                    E-recruitment System for Ministry of Agriculture Irrigation
+                    and Live-stock
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainFooter;
